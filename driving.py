7a15688644d1deb28d75e295ff533bf8e1699d47
longlylee <73825650+longlylee@users.noreply.github.com>
2020-11-04 03:13:24 +0800

    Update driving.py

--- driving.py
+++ driving.py
@@ -1,13 +1,13 @@
 country = input('请输入国籍')
 age = input('请输入年龄')
 age = int(age)
-if country == '中国'：
+if country == '中国':
     if age >= 18:
-    	print('你可以考驾照了')
-    elif：
+        print('你可以考驾照了')
+    else:
         print('你还不可以考驾照')
-elif country == '美国'：
-     if age >= 16:
-    	print('你可以考驾照了')
-    elif：
+elif country == '美国':
+    if age >= 16:
+        print('你可以考驾照了')
+    else:
         print('你还不可以考驾照')
